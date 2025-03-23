49f913d6e16a0195a1c0a9074d44449bf808d2da
SichevK228 <sichev_2001@mail.ru>
2025-03-23 19:20:02 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
 const express = require("express");
+const cors = require("cors");
 const axios = require("axios");
+
 const app = express();
 const port = process.env.PORT || 3000;
 
+app.use(cors());
 app.use(express.json());
 
 app.get("/products", async (req, res) => {
